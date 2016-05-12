f6848b807b672d4a8d8b04b20664824da49ff947
lgfng <747133768@qq.com>
2016-05-12 17:03:48 +0800

    change remote

--- filter_publish.lua
+++ filter_publish.lua
@@ -1,11 +1,13 @@
 local ROOT="~/advance/blog"
 local DRAFT_ROOT=ROOT .. "/source/_drafts/"
 
+local MIN_LINE_CNT = 15;
+
 for filename in io.popen("ls " .. DRAFT_ROOT):lines() do
     local r = io.popen("wc -l " .. DRAFT_ROOT .. filename)
     local str = r:read("*a")
     local lineCnt = string.match(str, "^(%w+)")
-    if tonumber(lineCnt) > 15 then
+    if tonumber(lineCnt) > MIN_LINE_CNT then
         local splitI = string.find(filename, "\.md")
         if splitI then
             filename = string.sub(filename, 1, splitI -1)
